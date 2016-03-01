0a44937c5014bb06ad1eea0c906de40f319ae7aa
Ethan Henderson <ethan@zbee.me>
2016-03-01 11:17:13 -0700

    Add more info to the dashboard

--- dash/index.php
+++ dash/index.php
@@ -5,9 +5,17 @@ $user = ditto::requireLogin();
 $user = ditto::getUser($user);
 ?>
 
-<div class="ribbon">
+<div class="ribbon" id="welcome">
   <div class="container">
-    <h1>Hello There!</h1>
+    <h1 class="bigbold">Welcome</h1>
+    <h1>To Your Dashboard</h1>
+    From here you can monitor your progress and log mood, sleep, and food.
+    <br><br>
+    <a class="button" href="/enter/mood/">Log Mood</a>
+    <a class="button" href="/enter/sleep/">Log Sleep</a>
+    <a class="button" href="/enter/food/">Log Food</a>
+  </div>
+</div>
   </div>
 </div>
 
