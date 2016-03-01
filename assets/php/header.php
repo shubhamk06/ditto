1ee2f1fa301495f9037fe6926ca4d497d4a44fa8
Ethan Henderson <ethan@zbee.me>
2016-03-01 10:35:26 -0700

    Seurity

--- assets/php/header.php
+++ assets/php/header.php
@@ -16,7 +16,7 @@ require_once(__DIR__ . "/loader.php");
     <link rel='stylesheet' type='text/css'
       href='https://fonts.googleapis.com/css?family=Open+Sans'>
     <link rel="stylesheet" type='text/css'
-      href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+      href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
     <link rel="stylesheet" type="text/css"
       href="https://ditto.zbee.me/assets/css/style.css">
   </head>
