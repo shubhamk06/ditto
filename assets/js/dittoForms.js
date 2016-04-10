3ebe739f92458bc962ab9941b051cab137e8d0e5
Ethan Henderson <ethan@zbee.me>
2016-04-10 14:51:13 -0600

    Base of #1 JS form file

--- assets/js/dittoForms.js
+++ assets/js/dittoForms.js
@@ -3,10 +3,13 @@
 //ditto forms library
 //variable/function name       | return  | comment
 //formID                       | string  | type of form being created
-//form                         | string  | form as it is being generated
+//section                      | string  | form section as it is being generated
 //answers                      | array<s>| array of default question answers
 //questions                    | array<s>| array of questions
 //responses                    | array<i>| array of responses provided
+//setup                        | object  | functions for form setup/construction
+//setup.fillIn                 | void    | filling in dittoForms variables
+//setup.verify                 | boolean | verify form is valid
 //function                     | object  | functions for form functionality
 //function.management          | void    | managing form responses
 //function.call                | void    | calling form's ditto functions
@@ -14,23 +17,75 @@
 //create                       | object  | functions for generating form
 //create.question              | void    | forming a question
 //create.answer                | void    | forming an answer
-//create.form                  | void    | forming the form, constructor
+//create.form                  | boolean | forming the form, constructor
 
 var dittoForms;
 dittoForms = {
   "formID"   : null,
-  "form"     : null,
+  "meta"     : null,
+  "section"  : null,
   "answers"  : null,
   "questions": null,
   "responses": null,
 
-  "function": {
+  "console": {
+    //Method for logging data to the console
+    "log": function () {
+      console.log("dittoForms.console.log()");
+      console.info(dittoForms);
+    }
+  },
+
+  "setup": {
+    //Method for filling in form attributes
+    "fillIn": function (json) {
+      console.log("dittoForms.setup.fillIn()");
+
+      dittoForms.meta      = json.form;
+      dittoForms.questions = json.questions;
+      dittoForms.answers   = json.answers;
+
+      dittoForms.console.log();
+    }, //Function to verify that the form provided was correct
+    "verify": function () {
+      console.log("dittoForms.setup.verify()");
+
+      var toReturn = false;
+
+      $.getJSON(
+        "https://ditto.zbee.me/assets/json/forms/"
+        + dittoForms.formID
+        + ".json", function (json) {
+          dittoForms.setup.fillIn(json);
+          toReturn = true;
+        }
+      ).fail(
+        function () {
+          toReturn = false;
+        }
+      );
+
+      dittoForms.console.log();
+      return toReturn;
+    }
+  },
+
+  "function": { //Methods for providing functionality to the form
     "management": {}, "call": {}, "callback": {}
   },
 
   "create": {
     "question": {}, "answer": {}, "form": function (formID, callback) {
+      console.log("dittoForms.create.form()");
 
+      dittoForms.formID = formID;
+      var verified      = dittoForms.setup.verify();
+      if (!verified) {
+        return false;
+      }
+
+      dittoForms.console.log();
+      return true;
     }
   }
 }
--- enter/handler.php
+++ enter/handler.php
@@ -3,5 +3,6 @@ require("../assets/php/header.php");
 $enter = $_SERVER["REQUEST_URI"];
 $enter = str_replace("/enter/", "", $enter);
 $enter = str_replace("/", "", $enter);
+echo "<script>dittoForms.create.form('phq9');</script>";
 require($enter . ".php");
 require("../assets/php/footer.php");
